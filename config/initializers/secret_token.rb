a07aaab468984b4b54d535b12e4d34cd138224fd
franko <francordie@gmail.com>
2012-05-23 16:00:45 -0400

    creating Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BiciNETa::Application.config.secret_token = '3e1203167c9ceec5831511dfad00add862d17e1938bb97ba3e6988bb47de88c562236cb6ee56a3221ae38b25efc5e19839449c99bfddd96a453a0ef4970a2e3a'
