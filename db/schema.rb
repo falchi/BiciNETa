99edf3d9945fb0025e3c0ce877105e2fc366f17e
franko <francordie@gmail.com>
2012-07-20 17:10:20 -0400

    change is_admin column type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120715064637) do
+ActiveRecord::Schema.define(:version => 20120720210615) do
 
   create_table "points", :force => true do |t|
     t.float    "latitude"
